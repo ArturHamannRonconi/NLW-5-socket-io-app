684c4d2d14aa0b38fcd9dc4fa0c916ed83ef7641
Artur Hamann Ronconi <arturhronconi@gmail.com>
2021-04-19 10:23:35 -0300

    create server and first route

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+
+const app = express()
+
+
+/**
+ * GET    = buscas
+ * POST   = criação
+ * PUT    = alteração
+ * DELETE = deletar
+ * PATCH  = alterar informação específica
+ */
+
+app.get('/', (request, response) => response.json({ message: 'Olá NLW#5' }))
+
+app.post('/', (request, response) => response.json({ message: 'Usuário salvo com sucesso' }))
+
+
+app.listen(3333, () => console.log('Server is running on port 33333...'))
