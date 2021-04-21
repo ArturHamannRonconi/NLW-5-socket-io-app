f8f65eddbaf3d12608e471960f208bd1d98643a2
Artur Hamann Ronconi <arturhronconi@gmail.com>
2021-04-21 16:35:16 -0300

    Entity Messages

---
+++ src/entities/Messages.ts
@@ -0,0 +1,35 @@
+import { Entity, PrimaryColumn, Column, CreateDateColumn, ManyToOne, JoinColumn } from 'typeorm'
+import { v4 as setUuid } from 'uuid'
+
+import User from './Users'
+
+@Entity('messages')
+class Messages
+{
+  @PrimaryColumn()
+  id: string
+
+  @Column()
+  admin_id: string
+  
+  @Column()
+  text: string
+
+  @JoinColumn({ name: 'user_id' })
+  @ManyToOne(() => User)
+  User: User
+
+  @Column()
+  user_id: string
+
+  @CreateDateColumn()
+  created_at: Date
+
+  constructor()
+  {
+    if(!this.id) this.id = setUuid()
+  }
+
+}
+
+export default Messages
