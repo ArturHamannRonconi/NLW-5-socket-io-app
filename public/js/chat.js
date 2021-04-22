a31eb9e7e1aac3e00dc77cd781ee392f9d39c5dc
Artur Hamann Ronconi <arturhronconi@gmail.com>
2021-04-22 20:07:12 -0300

    this directory  wasn't my job

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,23 @@
-document.querySelector("#start_chat").addEventListener("click", (event) => {});
+let socket
+
+document.querySelector("#start_chat").addEventListener("click", event => {
+  // const socket = io()
+  if(!socket) socket = io()
+
+  const chatHelp = document.querySelector('#chat_help')
+  chatHelp.style.display = 'none'
+    
+  const chatSupport = document.querySelector('#chat_in_support')
+  chatSupport.style.display = 'block'
+
+  const email = document.querySelector('#email').value
+  const text = document.querySelector('#txt_help').value
+  
+  socket.on('connect', () => {
+    socket.emit('clientFirstAccess', { email, text }, (call, err) => {
+      if(err) console.error(err)
+      else    console.log(call)
+    })
+  })
+
+});
