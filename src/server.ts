ed3e9266f8de0e7ee2ee65437d14499d177e408e
Artur Hamann Ronconi <arturhronconi@gmail.com>
2021-04-22 11:11:24 -0300

    add socket.io in server

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
+import { createServer } from 'http'
 import express from 'express'
+import { Server, Socket } from 'socket.io'
 
 import './database'
 import routes from './routes'
 
 const app = express()
 
+const http = createServer(app)
+const io = new Server(http)
+
+io.on('connection', (socket: Socket) => console.log(`Connection has been concluded... ${socket.id}`))
+
 app.use(express.json())
 app.use(routes)
 
-app.listen(3333, () => console.log('Server is running on port 33333...'))
+http.listen(3333, () => console.log('Server is running on port 33333...'))
